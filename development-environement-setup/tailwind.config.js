45f9ab741fa5b3447627494b38d224a60a339a39
ankur29mac <ankur29mac@gmail.com>
2023-04-21 00:08:22 +0530

    tailwind-cli + tailwind as postcss plugin setup

---
+++ development-environement-setup/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
